9780c4093ae0257e58c1bb991d9cf8447742b7ff
vincent-backend <yutaka@inc-acv.com>
2023-12-05 17:23:47 +0700

    piracy_detection.html complete

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,28 @@
 /* Piracy Detection */
 $("#btn-piracy-detection").click(function() {
+    let imageUrl = "/assets/images/piracy/popup_ic_Detection.svg";
+    $('.piracy-modal-img').css('background-image', 'url(' + imageUrl + ')');
+    $('.modal-subtitle').css({'color': '#05031A'});
+    $('.modal-subtitle').html("正在检测请勿关闭窗口…");
     $('#progModal1').modal('show');
+    
+    // simulate starting
+    setTimeout(function() {
+        $('.modal-subtitle').html("开始加载特征文件…");
+    }, 2000);
+
+    // simulate finishing
+    setTimeout(function() {
+        $('.modal-subtitle').html("特征文件加载完毕，请稍后…");
+    }, 4000);
+
+    // simulate loss
+    setTimeout(function() {
+        let imageUrl = "/assets/images/piracy/popup_ic_Detection_Lose.svg";
+        $('.piracy-modal-img').css('background-image', 'url(' + imageUrl + ')');
+        $('.modal-subtitle').css({'color': '#F22F43'});
+        $('.modal-subtitle').html("未检测到苹果CMS");
+    }, 6000);
 
 
 });
